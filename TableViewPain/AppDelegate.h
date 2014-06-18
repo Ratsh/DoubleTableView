021d55bab21bd8335cea315a6f75f11bc7780173
Ratsh <ratsha@mail.ru>
2014-06-18 22:11:03 +0400

    Initial Commit

---
+++ TableViewPain/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TableViewPain
+//
+//  Created by Admin on 18.06.14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
